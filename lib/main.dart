19f2e29e2818527682ecbadb86bba7c1b569decb
Denis Spetsialniy <luwirispok@gmail.com>
2023-03-18 02:02:01 +0300

    Added header excel

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:excel/excel.dart';
@@ -23,41 +24,70 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = '';
+
+  void names() async {
+    int count = 0;
+    var file = 'example_data/Аудит заявок РФ_13.03.23.xlsx';
+    var bytes = File(file).readAsBytesSync();
+    Excel excel = Excel.decodeBytes(bytes);
+    text = '[';
+    for (var table in excel.tables.keys) {
+      print(table); //sheet Name
+      print(excel.tables[table]!.maxCols);
+      print(excel.tables[table]!.maxRows);
+      // for (List<Data?> row in excel.tables[table]!.rows) {
+      //   for (var data in row) {
+      //     print(data?.value);
+      //     // print('$row');
+      //   }
+      // }
+      for (Data? element in excel.tables[table]!.rows[0]) {
+        // print(++count);
+
+        if (element == null) continue;
+        // log('${element.cellStyle}', name: '${element.value}');
+        // log('${element?.cellStyle?.backgroundColor}' ?? '', name: '${element?.value}');
+        // log('${element?.cellStyle?.props.last}' ?? '', name: '${element?.value}');
+
+        if (element.cellStyle!.backgroundColor != 'none') {
+          text += '\'${element.value}\',';
+          log('-------- ОНО!!!' ?? '', name: '${element.value}');
+        }
+        print('$text]');
+        setState(() {});
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    void names() async {
-      var file = 'C:/Users/Mikail/Desktop/audit.xlsx';
-      var bytes = await File(file).readAsBytesSync();
-      Excel excel = await Excel.decodeBytes(bytes);
-
-      for (var table in excel.tables.keys) {
-        print(table); //sheet Name
-        print(excel.tables[table]!.maxCols);
-        print(excel.tables[table]!.maxRows);
-        for (var row in excel.tables[table]!.rows) {
-          for (var data in row) {
-            print(data?.value);
-          }
-
-          // print('$row');
-        }
-      }
-    }
-
+    names();
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Center(
-            child: ElevatedButton(
-          onPressed: names,
-          child: Text('Press on me'),
-        )),
-      ],
-    ));
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: names,
+                child: Text('Press on me'),
+              ),
+            ),
+            Text(text),
+          ],
+        ),
+      ),
+    );
   }
 }
